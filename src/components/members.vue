86924d38738ec02694cfb64b6a987f481f6dbc95
Jeff Telford <jtelford@controlpad.com>
2017-05-23 22:52:05 -0600

    update to tab 1 in members only

--- src/components/members.vue
+++ src/components/members.vue
@@ -8,12 +8,121 @@
               </div>
             </div>
           </section>
-          <section class="padding-top-40 padding-bottom-40">
+          <section class="padding-top-10 padding-bottom-10">
             <div class="container">
               <div class="relative">
-                <div class="heading-block no-margin-bottom">
-                  <h4 class="margin-bottom-20 margin-top-0 letter-space-1">Coming Soon..</h4>
-                </div>
+                <!-- <div class="heading-block no-margin-bottom"> -->
+                  <!-- <h4 class="margin-bottom-20 margin-top-0 letter-space-1">Coming Soon..</h4> -->
+                  <ul class="nav nav-tabs">
+                      <li class="active"><a data-toggle="tab" href="#menu1">Living Doterra</a></li>
+                      <li><a data-toggle="tab" href="#menu2">Sharing Doterra</a></li>
+                      <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>
+                  </ul>
+                  <div class="tab-content">
+                      <div id="menu1" class="tab-pane fade in active">
+                          <div class="para1">
+                              <p>Welcome to Team Oil Magic! This tab will help you learn and get educated on all the products Doterra has to offer and how making a natural lifestyle can be easy, simply by education! Be sure to follow and like my <i><a href="https://www.facebook.com/oilmagicbymichelle" target="_blank">Facebook</a></i> page to watch weekly live classes that me and my top leaders teach, and also follow Team Oil Magic on Facebook as well for our private team page! I will approve you once you have added yourself! Explore this tab and learn all about these amazing products so you can dive in and start Living the Doterra lifestyle! Again, WELCOME to my amazing team!!! We are so blessed to have you!
+                              - Michelle
+                              </p>
+                          </div>
+                          <div class="safety">
+                              <h5>Product Information and Oil Safety Guidelines</h5>
+                              <p>Click <a href="https://doterra.com/US/en/university/living" target="_blank"><i>here</i></a> to read the product information and oil safety guidelines directly from Doterra! Be sure to check out the safety and uses content before you dive in! Safety is so important!</p>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Welcome to Doterra</h5>
+                              <div class="padding-top-5">
+                                  <iframe width="700" height="400" src="https://www.youtube.com/embed/M7btq8eYavY?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>What is a Doterra Essential Oil</h5>
+                              <div class="padding-top-5">
+                                  <iframe width="700" height="400" src="https://www.youtube.com/embed/OEnKOk4ad0U?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Why you should choose Doterra</h5>
+                              <div class="padding-top-5">
+                                  <iframe width="700" height="400" src="https://www.youtube.com/embed/o_M4XsYaOxM?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Lavender oil uses and benefits</h5>
+                              <div class="padding-top-5">
+                                  <iframe width="700" height="400" src="https://www.youtube.com/embed/wjbHnTrTeKc?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Lemon oil uses and benefits</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/nm6YPHuchCk?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Peppermint oil uses and benefits</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/DILz_VNQWlU?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Melaleuca oil uses and benefits</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/CPqUkdsy83A?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Digestzen oil uses and benefits</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/O5CpkASUQOo?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Oregano oil uses and benefits</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/uOh4CyEy2hU?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Deep Blue oil uses and benefits</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/aOhkMZxpE2k?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Daily Nutrient Pack (LLV)</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/pGjJ3SJ3ems?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Aromatouch Massage with Doterra Essential Oils</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/5OujhxQCgoY?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>Impacting lives with Doterra</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/Jbiu-Duwo2k?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                          <div class="welcome youtube-wrapper">
+                              <h5>LRP Program and Why you must participate!</h5>
+                              <div class="padding-top-5">
+                                 <iframe width="700" height="400" src="https://www.youtube.com/embed/QqV1tV3WK60?list=PLhVNYn5OGN22cjwEkp37x_MRgQD25a0a4" frameborder="0" allowfullscreen></iframe>
+                              </div>
+                          </div>
+                      </div>
+                      <div id="menu2" class="tab-pane fade">
+                          <p>Hi Michelle!</p>
+                      </div>
+                      <div id="menu3" class="tab-pane fade">
+                          <h3>Menu 3</h3>
+                          <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                      </div>
+                  </div>
+                <!-- </div> -->
                 <hr>
                 <div class="padding-top-20 center youtube-wrapper">
                    <!-- <iframe width="600" height="400" src="https://www.youtube.com/embed/8M9GkBJzrSM" frameborder="0" allowfullscreen></iframe> -->
@@ -31,6 +140,14 @@ export default {
 </script>
 
 <style lang="css">
+    .nav-tabs li {
+        width: 33%;
+        text-align: center;
+        font-size: 25px;
+    }
+    #menu1 .para1 {
+        margin-top: 10px;
+    }
     .members-only-wrapper h4 {
         font-weight: 300;
     }
