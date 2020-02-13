a777334332cc0824c9034b5956efe9b32ac56136
Jeff Telford <jefftelford@Jeff-Telford-MacBook-Pro.local>
2020-02-13 00:19:47 -0700

    instagram api test

--- src/resources/instagram.js
+++ src/resources/instagram.js
@@ -2,6 +2,7 @@ import Request from '../resources/requestHandler.js'
 
 export default {
   getPhotos: function () {
-    return Request.get('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=1558666427.1677ed0.ccfbaa5a60304eea99b6f69d31b913b5')
+    // return Request.get('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=1558666427.1677ed0.ccfbaa5a60304eea99b6f69d31b913b5')
+    return Request.get('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=329480.1677ed0.fe429ebbd0ce49bba2854a21c2490dd1')
   }
 }
